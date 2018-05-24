052f67da7960645a11a7bff6e1bb31ea39dc1287
Weibin <wy222ad@student.lnu.se>
2018-05-24 17:00:09 +0200

    1

--- t4_2.m
+++ t4_2.m
@@ -2,59 +2,92 @@ clc
 clear;
 close all;
 load('dataFeature.mat');
-X = table2array(features);
-mat = sammon(X,5000,100,1);
+X = table2array(features(1:200,:));
+mat = sammon(X,1000,100,1);
 
 function sam = sammon(X,iter,e,a)
     bestCost = 9999999;
     limit = size(X,2);
-    X_sum = sum(X,2);
-    %% keneng yaogai
-    in_sum = sum(X_sum,1);
+    bestY=[];
     for i = 1:iter
         r = randi([1 limit],1,2);
         while r(1)==r(2)
             r = randi([1 limit],1,2);
         end
         Y = [X(:,r(1)) X(:,r(2))];
-        currentCost = cost(X,Y,in_sum);
+        currentCost = cost(X,Y);
         if currentCost < bestCost
             bestCost = currentCost;
             bestY = Y;
         end
         if e<bestCost
-            i = iter;
+          	 break
         end
     end
     
-    o = one(X_sum,Y,in_sum);
+    o = one(X,bestY);
+    t = two(X,bestY);
+    tri = o/t;
     sam = bestCost;
 end
 
-function o = one(X_sum,Y,in_sum)
-    for i = 1:size(X_sum,1)
-        for ii = n:size(X_sum,1)
-            out_sum = out_sum + (X_sum(i)-Y(i))/X_sum(i);
+function t = two(X,Y)
+    in_sum = 0;
+    out_sum = 0;
+    for i = 1:size(X,1)
+        for ii = 1:size(X,1)
+            if i ~= ii
+                d = (Y(i,:)-Y(ii,:));
+                d = d.^2;
+                d = sqrt(sum(d,2));
+                b= (X(i,:)-X(ii,:));
+                b=b.^2;
+                b=sqrt(sum(b,2));
+                out2 = (1/(b-d))*((b-d)-(((Y(i,:)-Y(ii,:)).^2)/d)*...
+                    (1+((b-d)/d)));
+                out_sum = out_sum + out2;
+                in_sum = in_sum+b;
+            end
         end
-        n=n+1;
     end
-    o = -2;
+    t = (-2/in_sum)*out_sum;
 end
 
-function cos = cost(X,Y,in_sum)
+function o = one(X,Y)
     out_sum = 0;
+    in_sum =0;
+    for i = 1:size(X,1)
+        for ii = 1:size(X,1)
+            if i ~= ii
+                d = (Y(i,:)-Y(ii,:));
+                d = d.^2;
+                d = sqrt(sum(d,2));
+                b= (X(i,:)-X(ii,:));
+                b=b.^2;
+                b=sqrt(sum(b,2));
+                out1 = ((b-d)/(b*d))*(Y(i,:)-Y(ii,:));
+                out_sum = out_sum + out1;
+                in_sum = in_sum+b;
+            end
+        end
+    end
+    o = (-2/in_sum)*out_sum;
+end
+
+function cos = cost(X,Y)
+    out_sum = 0;
+    in_sum = 0;
     n = 2;
     for i = 1:size(X,1)
         for ii = n:size(X,1)
             d = (Y(i,:)-Y(ii,:));
             d = d.^2;
-            d = sum(d,2);
-            d= sqrt(d);
+            d= sqrt(sum(d,2));
             b= (X(i,:)-X(ii,:));
             b=b.^2;
-            b= sum(b,2);
-            d=sqrt(d);
-            out_sum = out_sum + ((d - b)^2)/ b;
+            b=sqrt(sum(b,2));
+            out_sum = out_sum + (((d - b)^2)/ b);
+            in_sum = in_sum+b;
         end
         n=n+1;
     end
